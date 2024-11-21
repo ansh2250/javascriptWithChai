c92a914148162c3f0ca15284e474357e6270406a
Ansh Kumar Sinha <165658758+ansh2250@users.noreply.github.com>
2024-11-21 11:58:11 +0000

    understanding datatypes

---
+++ 01Basic/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as a new version
+
+// alert ("Hello this is ansh") we are using node not browser
+
+console.log(3 + 
+    3) //code readiblity must be hight this is wrong readiblity
+
+    console.log("Ansh Kumar Sinha")
+
+    let userName = "shahzad"
+    let userAge = 18
+    let userLoggedIn = true
+    let state;
+
+    
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+    console.log([userName, userAge, userLoggedIn, state])
