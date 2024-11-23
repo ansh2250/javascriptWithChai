51115896049f0d3e199c7771be0d5c2b52102984
Ansh Kumar Sinha <165658758+ansh2250@users.noreply.github.com>
2024-11-23 06:29:46 +0000

    understanding conversion and opreation from basic level

---
+++ 01Basic/conversionOpreation.js
@@ -0,0 +1,63 @@
+let a = "Anas"
+// console.log(typeof a);
+
+let newA = Number(a)
+console.log(newA);
+
+let studentName = 122
+// console.log(typeof studentName);
+
+let newStudentName = String(studentName)
+
+console.log(newStudentName);
+
+let studentAttendance = "50%"
+
+console.log(typeof studentAttendance)
+
+let newStudentAttendance = Boolean(studentAttendance)
+
+// console.log(typeof(newStudentAttendance));
+
+console.log(typeof(newStudentAttendance));
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
